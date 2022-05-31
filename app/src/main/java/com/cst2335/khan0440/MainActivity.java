226b6c026b5b1577aa336b350be7d9cfe214c53c
Hamza-Khan-algoqnuin <khan0440@algonquinlive.com>
2022-05-31 16:42:00 -0400

    section 1 with changing flag

--- app/src/main/java/com/cst2335/khan0440/MainActivity.java
+++ app/src/main/java/com/cst2335/khan0440/MainActivity.java
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
-import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -23,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linearlayout);
 
         Button mButton = (Button) findViewById(R.id.button2);mButton.setOnClickListener(new View.OnClickListener() {
             @Override
